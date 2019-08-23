6e7892bc68c69b30679de81db63d43f2e1fed155
Diego <zarek46664@gmail.com>
2019-08-23 10:17:46 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#
+
+using namespace std;
+
+
+
+
+int main (){
+
+int a,b,c ;
+
+a=45;
+b=145;
+c=23;
+
+if (a<b and a < c)
+
+	cout << " El numero menor es" << a << endl:
+if (b<a and b < c)
+
+	cout << " El numero menor es" << b << endl:
+else
+
+	cout << " El numero menor es" << c << endl:
+
+	return 0;
+}
